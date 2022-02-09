3ae9df96d047c083df84971af4f59a52ab8b98b8
trymtv <tiltrym@gmail.com>
2022-02-09 17:44:54 +0100

    first commit

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page-header">
+    <slot/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PageHeader',
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/page-header";
+
+</style>
